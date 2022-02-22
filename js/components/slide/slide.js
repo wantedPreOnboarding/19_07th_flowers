e81120c3779e6f1a5fae05fc292086e391a833bc
Sooim Choi <weniger2021@gmail.com>
2022-02-22 21:56:46 +0900

    Add slide 버튼 js 연결

---
+++ js/components/slide/slide.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".next__btn").click(function() {		// 모든 <button>요소에 click 이벤트를 설정함.
+      alert("앞으로가기 버튼")
+    });
+    $(".prev__btn").click(function() {		// 모든 <button>요소에 click 이벤트를 설정함.
+        alert("뒤로가기 버튼!")
+      });
+});
