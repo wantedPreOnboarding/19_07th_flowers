e469dd5dba4aebce7a7c72f2ed7bc84796938b7a
yeongjong310 <tohero21@naver.com>
2022-02-22 17:17:16 +0900

    Add - 가독성을 위한 띄어쓰기

--- pages/popUp/js/main.js
+++ pages/popUp/js/main.js
@@ -1,6 +1,7 @@
 (function () {
   "use strict";
   var vh = require("vh");
+
   $(window).on("load", function () {
     var footerHeight = $(".popup__footer").outerHeight(true) - 1;
     $(".popup-slide__wrapper").css("height", 100 * vh - footerHeight);
