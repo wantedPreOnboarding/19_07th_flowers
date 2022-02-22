49f0f4fdb16e3fb237a62853c434a4e618e35617
Sooim Choi <weniger2021@gmail.com>
2022-02-23 03:26:05 +0900

    Add slide 버튼 클릭시 컨텐츠 변경 추가

--- js/components/slide/slide.js
+++ js/components/slide/slide.js
@@ -1,39 +1,55 @@
 $(document).ready(function() {
-  // var slideList = [{imageId:"1", imageName:"클래식 핸드타이드(1/1~1/15)"}, {imageId:"2", imageName:"들꽃 화병 꽃이(1/10)~(1/16)"}, {imageId:"3",  imageName:"롬앤로우 센터피스 (1/17)~(1/23)"}]   
-  var state=0;
+  var tagList=[
+    {store:"광화문점",color:"#8A2BE2"},
+    {store:"잠실점",color:"#1DD1AD"},
+    {store:"월계점",color:"#FF9100"},
+    {store:"구로점",color:"#0A82FF"},
+    {store:"송파점",color:"#FFBE0A"},
+    {store:"부산동래점",color:"#46BEFF"}]
+  
+  for (i=0; i<tagList.length; i++){
+    console.log(tagList[i].store);
+    var $span=$("<span></span>").css("margin-right","5px").css("border","1px solid "+tagList[i].color).css("color",tagList[i].color).text(tagList[i].store);
+    $(".tag__box").append($span)
+  }
+
+  var slideList = [{imageId:"0", imageName:"클래식 핸드타이드(1/1 ~ 1/15)"}, {imageId:"1", imageName:"들꽃 화병꽂이(1/10 ~ 1/16)"}, {imageId:"2",  imageName:"롬앤로우 센터피스 (1/17 ~ 1/23)"}]   
+  var state=1;
+
+  function changeImage(first,second,third){
+    $(".slide--image__active").attr("src","assets/slide_image"+first+".jpeg"),
+    $(".slide--image2").attr("src","assets/slide_image"+second+".jpeg"),
+    $(".slide--image3").attr("src","assets/slide_image"+third+".jpeg"),
+    $(".slide__content--firstTitle").text(slideList[second].imageName),
+    $(".slide__content--secTitle").text(slideList[third].imageName)
+  }
+
   $(".next__btn").click(function() {
     if (state ===2){
-      $(".slide--image__active").attr("src","assets/slide_image"+state+".jpeg"),
-      $(".slide--image2").attr("src","assets/slide_image"+(state-2)+".jpeg"),
-      $(".slide--image3").attr("src","assets/slide_image"+(state-1)+".jpeg"),
+      changeImage(state,state-2,state-1),
       state=0
     }
     else if(state ===1){
-      $(".slide--image__active").attr("src","assets/slide_image"+state+".jpeg"),
-      $(".slide--image2").attr("src","assets/slide_image"+(state+1)+".jpeg"),
-      $(".slide--image3").attr("src","assets/slide_image"+(state-1)+".jpeg"),
+      changeImage(state,state+1,state-1),
       state++
     }
     else{
-      $(".slide--image__active").attr("src","assets/slide_image"+state+".jpeg"),
-      $(".slide--image2").attr("src","assets/slide_image"+(state+1)+".jpeg"),
-      $(".slide--image3").attr("src","assets/slide_image"+(state+2)+".jpeg"),
+      changeImage(state,state+1,state+2),
       state++
     } 
-});
-  $(".prev__btn").click(function() {		// 모든 <button>요소에 click 이벤트를 설정함.
-      alert("뒤로가기 버튼!")
+  });
+  $(".prev__btn").click(function() {	
+    if (state ===2){
+      changeImage(state-1,state,state-2)
+      state=0
+    }
+    else if(state ===1){
+      changeImage(state+1,state-1,state),
+      state++
+    }
+    else{
+      changeImage(state,state+1,state+2),
+      state++
+    } 
   });
 });
-
-
-    
-    // if (state ===2){
-    //   state=0
-    // }
-    // else{
-    //   state++
-    // }
-    // $(".slide--image__active").attr("src",`assets/slide_image${state}.jpeg`),
-    // $(".slide--image2").attr("src",`assets/slide_image${state===2?state-2:state+1}.jpeg`),
-    // $(".slide--image3").attr("src",`assets/slide_image${state!==0 ?state-1:state+2}.jpeg`)
