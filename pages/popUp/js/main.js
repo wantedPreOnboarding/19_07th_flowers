8c5f43ae212e1065a19b78d959fcff0dffb5fc06
yeongjong310 <tohero21@naver.com>
2022-02-22 20:29:29 +0900

    Add - 슬라이드기능 구현 및 푸터 디자인

--- pages/popUp/js/main.js
+++ pages/popUp/js/main.js
@@ -2,8 +2,39 @@
   "use strict";
   var vh = require("vh");
 
+  var IMGS = [
+    "/pages/popUp/assets/20220221_배송안내.png",
+    "/pages/popUp/assets/20220221_배송안내.png",
+    "/pages/popUp/assets/20220221_배송안내.png",
+  ];
+
+  var currentIndex = 0;
+
+  var setCurrentIndex = function (nextIndex) {
+    if (nextIndex < 0 || nextIndex > IMGS.length - 1) return;
+    currentIndex = nextIndex;
+    popupImageRender();
+  };
+
+  var $slideWrapper = $(".popup-slide__wrapper");
+
+  function popupImageRender() {
+    $slideWrapper.find("ul").animate({
+      right: $slideWrapper.find("img").width() * currentIndex,
+    });
+  }
+
   $(window).on("load", function () {
-    var footerHeight = $(".popup__footer").outerHeight(true) - 1;
-    $(".popup-slide__wrapper").css("height", 100 * vh - footerHeight);
+    $slideWrapper.find("img").css({
+      height: 100 * vh - $(".popup__footer").height() - 5,
+    });
+
+    $("[class*=popup-slide__btn]").on("click", function () {
+      if (this.getAttribute("class").indexOf("next") !== -1) {
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(currentIndex - 1);
+      }
+    });
   });
 })();
