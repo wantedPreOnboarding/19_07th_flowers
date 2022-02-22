4717c278db35982c57aa3b8abe277c2487b525d0
yeongjong310 <tohero21@naver.com>
2022-02-22 20:57:47 +0900

    Add - popup 슬라이드의 현재 인덱스 표시 기능 구현

--- pages/popUp/js/main.js
+++ pages/popUp/js/main.js
@@ -7,21 +7,40 @@
     "/pages/popUp/assets/20220221_배송안내.png",
     "/pages/popUp/assets/20220221_배송안내.png",
   ];
+  var indexManager = (function () {
+    var currentIndex = 0;
 
-  var currentIndex = 0;
+    var getCurrentIndex = function () {
+      return currentIndex;
+    };
 
-  var setCurrentIndex = function (nextIndex) {
-    if (nextIndex < 0 || nextIndex > IMGS.length - 1) return;
-    currentIndex = nextIndex;
-    popupImageRender();
-  };
+    var setCurrentIndex = function (nextIndex) {
+      if (nextIndex < 0 || nextIndex > IMGS.length - 1) return;
+      currentIndex = nextIndex;
+      popupImageRender();
+      indexViewerRender();
+    };
+
+    return {
+      get: getCurrentIndex,
+      set: setCurrentIndex,
+    };
+  })();
 
   var $slideWrapper = $(".popup-slide__wrapper");
 
   function popupImageRender() {
     $slideWrapper.find("ul").animate({
-      right: $slideWrapper.find("img").width() * currentIndex,
+      right: $slideWrapper.find("img").width() * indexManager.get(),
     });
+  }
+
+  function indexViewerRender() {
+    var $circles = $(".popup-slide__index-viewer > span");
+    $circles.removeClass("bgc-red");
+    $circles.addClass("bgc-grey");
+
+    $circles.eq(indexManager.get()).addClass("bgc-red");
   }
 
   $(window).on("load", function () {
@@ -31,9 +50,9 @@
 
     $("[class*=popup-slide__btn]").on("click", function () {
       if (this.getAttribute("class").indexOf("next") !== -1) {
-        setCurrentIndex(currentIndex + 1);
+        indexManager.set(indexManager.get() + 1);
       } else {
-        setCurrentIndex(currentIndex - 1);
+        indexManager.set(indexManager.get() - 1);
       }
     });
   });
