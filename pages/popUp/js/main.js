2308d624ee25f621f4c80064cab9cafcc67276e8
yeongjong310 <tohero21@naver.com>
2022-02-23 00:19:58 +0900

    Add - 팝업에서 slide의 index 관련 처리 모듈 분리

--- pages/popUp/js/main.js
+++ pages/popUp/js/main.js
@@ -1,48 +1,7 @@
 (function () {
   "use strict";
   var vh = require("vh");
-
-  var IMGS = [
-    "/pages/popUp/assets/20220221_배송안내.png",
-    "/pages/popUp/assets/20220221_배송안내.png",
-    "/pages/popUp/assets/20220221_배송안내.png",
-  ];
-
-  var indexManager = (function () {
-    var currentIndex = 0;
-
-    var getCurrentIndex = function () {
-      return currentIndex;
-    };
-
-    var setCurrentIndex = function (nextIndex) {
-      if (nextIndex < 0 || nextIndex > IMGS.length - 1) return;
-      currentIndex = nextIndex;
-      popupImageRender();
-      indexViewerRender();
-    };
-
-    return {
-      get: getCurrentIndex,
-      set: setCurrentIndex,
-    };
-
-    function popupImageRender() {
-      var $slideWrapper = $(".popup-slide__wrapper");
-
-      $slideWrapper.find("ul").animate({
-        right: $slideWrapper.find("img").width() * indexManager.get(),
-      });
-    }
-
-    function indexViewerRender() {
-      var $circles = $(".popup-slide__index-viewer > button");
-      $circles.removeClass("bgc-red");
-      $circles.addClass("bgc-grey");
-
-      $circles.eq(indexManager.get()).addClass("bgc-red");
-    }
-  })();
+  var slideIndexManager = require("slideIndexManager");
 
   $(window).on("load", function () {
     $(".popup-slide__wrapper")
@@ -53,9 +12,9 @@
 
     $("[class*=popup-slide__btn]").on("click", function () {
       if (this.getAttribute("class").indexOf("next") !== -1) {
-        indexManager.set(indexManager.get() + 1);
+        slideIndexManager.set(slideIndexManager.get() + 1);
       } else {
-        indexManager.set(indexManager.get() - 1);
+        slideIndexManager.set(slideIndexManager.get() - 1);
       }
     });
 
@@ -66,7 +25,7 @@
         .find("button")
         .index($(event.target));
 
-      indexManager.set(selectedIndex);
+      slideIndexManager.set(selectedIndex);
     });
 
     $(".popup__close").on("click", function () {
