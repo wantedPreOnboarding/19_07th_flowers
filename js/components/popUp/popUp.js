6aab814dbcc1192b637c575cfe630e004a6c9d6e
yeongjong310 <tohero21@naver.com>
2022-02-23 01:01:00 +0900

    Add - popup 위치 설정

--- js/components/popUp/popUp.js
+++ js/components/popUp/popUp.js
@@ -5,8 +5,31 @@
     var popupWindow = null;
 
     function open(href, width, height) {
-      var size = "width=" + width + "," + "height=" + height;
-      popupWindow = window.open(href, "", size);
+      var windowWidth = window.innerWidth
+        ? window.innerWidth
+        : document.documentElement.clientWidth
+        ? document.documentElement.clientWidth
+        : screen.width;
+
+      var windowHeight = window.innerHeight
+        ? window.innerHeight
+        : document.documentElement.clientHeight
+        ? document.documentElement.clientHeight
+        : screen.height;
+
+      var left = windowWidth / 2 - width / 2;
+
+      // var top = windowHeight / 2 - height / 2;
+
+      var popUpWidth = "width=" + width;
+      var popUpheight = "height=" + height;
+      var popUpLeft = "left=" + -left;
+
+      popupWindow = window.open(
+        href,
+        "",
+        [popUpWidth, popUpheight, popUpLeft].join(",")
+      );
     }
 
     function close() {
